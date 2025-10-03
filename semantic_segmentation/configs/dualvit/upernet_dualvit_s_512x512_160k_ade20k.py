f58be48719c2b843d2fc9564431bd2597cf11049
Mohamed Sayhi <mohamedsayhii@outlook.com>
2025-10-03 10:18:20 +0100

    Use 20k iter for the moment

--- semantic_segmentation/configs/dualvit/upernet_dualvit_s_512x512_160k_ade20k.py
+++ semantic_segmentation/configs/dualvit/upernet_dualvit_s_512x512_160k_ade20k.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/upernet_dualvit.py', '../_base_/datasets/cade20k.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
 ]
 model = dict(
     backbone=dict(
