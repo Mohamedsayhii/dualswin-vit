062137b82f841fb4db8cf2cab1f64e69572ada84
Mohamed Sayhi <mohamedsayhii@outlook.com>
2025-10-04 15:10:52 +0100

    Revert to cade and 160k iter

--- semantic_segmentation/configs/dualvit/upernet_dualvit_s_512x512_160k_ade20k.py
+++ semantic_segmentation/configs/dualvit/upernet_dualvit_s_512x512_160k_ade20k.py
@@ -1,6 +1,6 @@
 _base_ = [
-    '../_base_/models/upernet_dualvit.py', '../_base_/datasets/ade20k.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+    '../_base_/models/upernet_dualvit.py', '../_base_/datasets/cade20k.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
 ]
 model = dict(
     backbone=dict(
